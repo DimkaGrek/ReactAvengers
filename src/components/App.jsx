88d2744161d0c377a027021630514e1c647e7276
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-02-08 16:03:10 +0200

    config 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+const App = () => {
+  return <div>React Avengers</div>;
 };
+
+export default App;
