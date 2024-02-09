e83a23715aede22b511796aa83336ac8a738472c
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-02-09 19:45:36 +0200

    custom Select

--- src/components/UserSetsModal/UserSetsForm/UserSetsForm.jsx
+++ src/components/UserSetsModal/UserSetsForm/UserSetsForm.jsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
+import { UserSetsFormSelect } from './UserSetsFormSelect/UserSetsFormSelect';
 import s from './UserSetsForm.module.css';
 
 export const UserSetsForm = () => {
+  const [name, setName] = useState('default');
+  const [currency, setCurrency] = useState('uah');
+  const handleChange = e => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(currency, name);
+  };
+
   return (
-    <form className={s.form}>
+    <form onSubmit={handleSubmit} className={s.form}>
       <div className={s.formWrapper}>
-        <select className={s.select} name="currency " id="currency">
-          <option value="uan">₴ UAH</option>
-          <option value="usd">₴ USD</option>
-          <option value="eur">₴ EUR</option>
-        </select>
-        <input className={s.input} type="text" />
+        <UserSetsFormSelect setCurrency={setCurrency} />
+        <input
+          className={s.input}
+          onChange={handleChange}
+          type="text"
+          name="name"
+          value={name}
+        />
       </div>
       <button className={s.btnSubmit}>Save</button>
     </form>
