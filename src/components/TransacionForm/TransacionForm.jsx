f32c2d84b696e83c512b870a0f09742ac1f9882d
Sergey Kabanets <skabanets97@gmail.com>
2024-02-09 20:42:52 +0200

    add transaction form template

---
+++ src/components/TransacionForm/TransacionForm.jsx
@@ -0,0 +1,62 @@
+import { Icon } from 'components';
+import s from './TransacionForm.module.css';
+
+export const TransacionForm = () => {
+  return (
+    <form className={s.transacionForm}>
+      <div className={s.transactionTypes}>
+        <label className={s.typeLabel}>
+          <input
+            className={s.radioBtn}
+            type="radio"
+            name="transaction-type"
+            value="expense"
+            checked
+          />
+          <span className={s.customRadioBtn}></span>
+          Expense
+        </label>
+        <label className={s.typeLabel}>
+          <input
+            className={s.radioBtn}
+            type="radio"
+            name="transaction-type"
+            value="income"
+          />
+          <span className={s.customRadioBtn}></span>
+          Income
+        </label>
+      </div>
+      <div className={s.dateFields}>
+        <div className={s.fieldWrapper}>
+          <label>Date</label>
+          <input type="date" name="date" placeholder="mm/dd/yyyy" />
+        </div>
+        <div className={s.fieldWrapper}>
+          <label>Time</label>
+          <input type="time" name="time" />
+        </div>
+      </div>
+      <div className={s.fieldWrapper}>
+        <label>Category</label>
+        <input type="text" name="category" placeholder="Different" />
+      </div>
+      <div className={s.fieldWrapper}>
+        <label>Sum</label>
+        <input type="number" name="sum" placeholder="Enter the sum" />
+      </div>
+      <div className={s.fieldWrapper}>
+        <label>Comment</label>
+        <textarea
+          className={s.comment}
+          name="comment"
+          placeholder="Enter the text"
+        />
+      </div>
+      <button className={s.submitBtn} type="submit">
+        Add
+      </button>
+      <Icon name="close-btn" />
+    </form>
+  );
+};
