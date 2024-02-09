e4219cb3f499ef080667a952e941e3d2ff1b390a
Sergey Kabanets <skabanets97@gmail.com>
2024-02-09 12:10:51 +0200

    add reexport for icon component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as SharedLayout } from './SharedLayout/SharedLayout.jsx';
 export { default as Header } from './Header/Header.jsx';
 export * from './ModalContent/ModalContext.jsx';
 export * from './Modal/Modal.jsx';
+export * from './Icon/Icon.jsx';
