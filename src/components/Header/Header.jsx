57f72cc927f7ff7b58a23bf67fc05f7dde375405
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-02-10 15:08:34 +0200

    added logout user logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import headerStyles from './headerStyles.module.css';
 import Logo from './Logo/Logo';
 import BurgerMenuBtn from './BurgerMenuBtn/BurgerMenuBtn';
